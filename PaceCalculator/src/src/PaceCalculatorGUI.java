094f4ff2604baacc8d5084a37efb1a44a77f70d0
Thomas Bolinger <thomasbolinger@yahoo.com>
2016-10-27 11:35:13 -0400

    Initial project setup

---
+++ PaceCalculator/src/src/PaceCalculatorGUI.java
@@ -0,0 +1,16 @@
+package src;
+
+/*
+ * Created 10-26-16 by Thomas Bolinger
+ */
+public class PaceCalculatorGUI {
+
+	/**
+	 * Main method to create and run PaceCalculatorGUI
+	 */
+	public static void main(String[] args){
+		System.out.println("Basic project setup.");
+	}
+}
+
+
